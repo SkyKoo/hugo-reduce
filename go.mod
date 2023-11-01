45ab1cd798a0e3f3cc540c9d4012bc3a769b878c
SkyKoo <guchengming@live.cn>
2023-11-01 16:01:09 +0800

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SkyKoo/hugo-reduce
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+  "fmt"
+  "github.com/SkyKoo/hugo-reduce/log"
+)
+
+func main() {
+  // 0. local example contents
+
+  log.Process("main", "prepare example project file systems")
+
+  fmt.Println("===temp dir at last > ...")
+}
