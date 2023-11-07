d855598942c8a7ac0dc6faf46212ea82001ffb25
SkyKoo <guchengming@live.cn>
2023-11-07 15:54:56 +0800

    update

---
+++ common/paths/path.go
@@ -0,0 +1,12 @@
+package paths
+
+import "path/filepath"
+
+// AbsPathify creates an absolute path if given a working dir and a relative path.
+// If already absolute, the path is just cleaned.
+func AbsPathify(workingDir, inPath string) string {
+  if filepath.IsAbs(inPath) {
+    return filepath.Clean(inPath)
+  }
+  return filepath.Join(workingDir, inPath)
+}
