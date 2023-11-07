de150967256aef0d5fcb8a1f15f2e9210105039f
SkyKoo <guchengming@live.cn>
2023-11-07 16:02:56 +0800

    update

--- common/paths/path.go
+++ common/paths/path.go
@@ -1,6 +1,8 @@
 package paths
 
-import "path/filepath"
+import (
+  "path/filepath"
+)
 
 // AbsPathify creates an absolute path if given a working dir and a relative path.
 // If already absolute, the path is just cleaned.
